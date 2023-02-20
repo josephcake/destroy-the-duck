dd16ce654e9505042f005b133d456ff5c9db21d8
joseph gao <joseph8p@gmail.com>
2023-02-20 13:22:50 -0800

    knowndirectionhelper

---
+++ src/Context/algorithms/knownDirectionHelper.js
@@ -0,0 +1,304 @@
+import { checkValidCells } from './checkValidCellsHelper';
+
+export const knownDirectionHelper = (starting, ending) => {
+	let path = {};
+	let queue = [];
+	let eR = ending.split('-')[0];
+	let eC = ending.split('-')[1];
+	const helper = (current) => {
+		let validCells = checkValidCells(current);
+		let {
+			currentCell,
+			upCell,
+			downCell,
+			leftCell,
+			rightCell,
+			upNext,
+			downNext,
+			leftNext,
+			rightNext,
+			cR,
+			cC,
+		} = validCells;
+
+		if (
+			leftNext === ending ||
+			rightNext === ending ||
+			downNext === ending ||
+			upNext === ending
+		) {
+			return queue;
+		}
+
+		const shouldGoRight = cC < eC;
+		const shouldGoLeft = cC > eC;
+		const shouldGoUp = cR > eR;
+		const shouldGoDown = cR < eR;
+
+		if (shouldGoRight) {
+			if (rightCell && !path[rightNext]) {
+				current = rightNext.split('-');
+				path[rightNext] = true;
+				queue.push(rightNext);
+				return helper(rightNext);
+			} else {
+				const potentialPaths = Object.keys(path);
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					rightNext = `${cR}-${cC + 1}`;
+					rightCell = document.getElementById(rightNext);
+					if (rightCell && !path[rightNext]) {
+						current = rightNext.split('-');
+						path[rightNext] = true;
+						queue.push(rightNext);
+						return helper(rightNext);
+					}
+				}
+
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					// rightNext = `${cR}-${cC + 1}`;
+					// rightCell = document.getElementById(rightNext);
+					upNext = `${cR - 1}-${cC}`;
+					downNext = `${cR + 1}-${cC}`;
+					upCell = document.getElementById(upNext);
+					downCell = document.getElementById(downNext);
+					// if (rightCell && !path[rightNext]) {
+					// 	current = rightNext.split('-');
+					// 	path[rightNext] = true;
+					// queue.push(path);
+					// 	return helper(path);
+					// }
+					if (upCell && !path[upNext]) {
+						current = upNext.split('-');
+						path[upNext] = true;
+						queue.push(upNext);
+						return helper(upNext);
+					}
+					if (downCell && !path[downNext]) {
+						current = downNext.split('-');
+						path[downNext] = true;
+						queue.push(downNext);
+						return helper(downNext);
+					}
+				}
+
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					leftNext = `${cR}-${cC - 1}`;
+					leftCell = document.getElementById(leftNext);
+					if (leftCell && !path[leftNext]) {
+						current = leftNext.split('-');
+						path[leftNext] = true;
+						queue.push(leftNext);
+						return helper(leftNext);
+					}
+				}
+			}
+		} else if (shouldGoLeft) {
+			if (leftCell && !path[leftNext]) {
+				current = leftNext.split('-');
+				path[leftNext] = true;
+				queue.push(leftNext);
+				return helper(leftNext);
+			} else {
+				const potentialPaths = Object.keys(path);
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					// upNext = `${cR - 1}-${cC}`;
+					// downNext = `${cR + 1}-${cC}`;
+					// upCell = document.getElementById(upNext);
+					// downCell = document.getElementById(downNext);
+					leftNext = `${cR}-${cC - 1}`;
+					leftCell = document.getElementById(leftNext);
+					if (leftCell && !path[leftNext]) {
+						current = leftNext.split('-');
+						path[leftNext] = true;
+						queue.push(leftNext);
+						return helper(leftNext);
+					}
+					// if (upCell && !path[upNext]) {
+					// 	current = upNext.split('-');
+					// 	path[upNext] = true;
+					// queue.push(path);
+					// 	return helper(path);
+					// }
+					// if (downCell && !path[downNext]) {
+					// 	current = downNext.split('-');
+					// 	path[downNext] = true;
+					// queue.push(path);
+					// 	return helper(path);
+					// }
+				}
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					upNext = `${cR - 1}-${cC}`;
+					downNext = `${cR + 1}-${cC}`;
+					upCell = document.getElementById(upNext);
+					downCell = document.getElementById(downNext);
+					// leftNext = `${cR}-${cC - 1}`;
+					// leftCell = document.getElementById(leftNext);
+					// if (leftCell && !path[leftNext]) {
+					// 	current = leftNext.split('-');
+					// 	path[leftNext] = true;
+					// queue.push(path);
+					// 	return helper(path);
+					// }
+					if (upCell && !path[upNext]) {
+						current = upNext.split('-');
+						path[upNext] = true;
+						queue.push(upNext);
+						return helper(upNext);
+					}
+					if (downCell && !path[downNext]) {
+						current = downNext.split('-');
+						path[downNext] = true;
+						queue.push(downNext);
+						return helper(downNext);
+					}
+				}
+
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					rightNext = `${cR}-${cC + 1}`;
+					rightCell = document.getElementById(rightNext);
+					if (rightCell && !path[rightNext]) {
+						current = rightNext.split('-');
+						path[rightNext] = true;
+						queue.push(rightNext);
+						return helper(rightNext);
+					}
+				}
+			}
+		}
+		if (shouldGoUp) {
+			if (upCell && !path[upNext]) {
+				current = upNext.split('-');
+				path[upNext] = true;
+				queue.push(upNext);
+				return helper(upNext);
+			} else {
+				const potentialPaths = Object.keys(path);
+
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					upNext = `${cR - 1}-${cC}`;
+					upCell = document.getElementById(upNext);
+					if (upCell && !path[upNext]) {
+						current = upNext.split('-');
+						path[upNext] = true;
+						queue.push(upNext);
+						return helper(upNext);
+					}
+				}
+
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					leftNext = `${cR}-${cC - 1}`;
+					leftCell = document.getElementById(leftNext);
+					rightNext = `${cR}-${cC + 1}`;
+					rightCell = document.getElementById(rightNext);
+
+					if (leftCell && !path[leftNext]) {
+						current = leftNext.split('-');
+						path[leftNext] = true;
+						queue.push(leftNext);
+						return helper(leftNext);
+					} else if (rightCell && !path[rightNext]) {
+						current = rightNext.split('-');
+						path[rightNext] = true;
+						queue.push(rightNext);
+						return helper(rightNext);
+					}
+				}
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					downNext = `${cR + 1}-${cC}`;
+					downCell = document.getElementById(downNext);
+					if (downCell && !path[downNext]) {
+						current = downNext.split('-');
+						path[downNext] = true;
+						queue.push(downNext);
+						return helper(downNext);
+					}
+				}
+			}
+		} else if (shouldGoDown) {
+			if (downCell && !path[downNext]) {
+				current = downNext.split('-');
+				path[downNext] = true;
+				queue.push(downNext);
+				return helper(downNext);
+			} else {
+				const potentialPaths = Object.keys(path);
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					downNext = `${cR + 1}-${cC}`;
+					downCell = document.getElementById(downNext);
+					if (downCell && !path[downNext]) {
+						current = downNext.split('-');
+						path[downNext] = true;
+						queue.push(downNext);
+						return helper(downNext);
+					}
+				}
+
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					leftNext = `${cR}-${cC - 1}`;
+					leftCell = document.getElementById(leftNext);
+					rightNext = `${cR}-${cC + 1}`;
+					rightCell = document.getElementById(rightNext);
+					if (leftCell && !path[leftNext]) {
+						current = leftNext.split('-');
+						path[leftNext] = true;
+						queue.push(leftNext);
+						return helper(leftNext);
+					} else if (rightCell && !path[rightNext]) {
+						current = rightNext.split('-');
+						path[rightNext] = true;
+						queue.push(rightNext);
+						return helper(rightNext);
+					}
+				}
+				for (let i = potentialPaths.length - 1; i >= 0; i--) {
+					current = potentialPaths[i].split('-');
+					cR = Number(current[0]);
+					cC = Number(current[1]);
+					upNext = `${cR - 1}-${cC}`;
+					upCell = document.getElementById(upNext);
+					if (upCell && !path[upNext]) {
+						current = upNext.split('-');
+						path[upNext] = true;
+						queue.push(upNext);
+						return helper(upNext);
+					}
+				}
+			}
+		}
+	};
+	helper(starting);
+	return queue;
+};
